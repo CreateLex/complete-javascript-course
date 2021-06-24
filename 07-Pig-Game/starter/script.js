aec87bab852e2b57d7bc16290b74602b32776f29
Createlex <info@createLex.com>
2021-06-23 22:19:43 -0700

    Pig Commit
    Added CSS none
    and reset score and hide dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+
+const scoreElement0 = document.querySelector('#score--0');
+const scoreElement1 = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+
+scoreElement0.textContent = 0;
+scoreElement1.textContent = 0;
+diceElement.classList.add('hidden');
+
+
+
+const btnRole = document.querySelector('.btn--roll');
+const diceImage = document.querySelector('.dice');
+
+btnRole.addEventListener('click', function () {
+
+    const MAXNUMBER = 6;
+    const randomNumber = Math.floor((Math.random() * MAXNUMBER) + 1);
+    console.log(diceImage.getElementsByClassName('dice'));
+});
