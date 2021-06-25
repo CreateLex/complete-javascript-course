9631e02b9ba9df4c620c8e14f69a55e5f660b08c
Createlex <info@createLex.com>
2021-06-24 20:03:29 -0700

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -6,6 +6,7 @@ const scoreElement1 = document.getElementById('score--1');
 const diceElement = document.querySelector('.dice');
 
 
+// initializes variables and reset game
 scoreElement0.textContent = 0;
 scoreElement1.textContent = 0;
 diceElement.classList.add('hidden');
