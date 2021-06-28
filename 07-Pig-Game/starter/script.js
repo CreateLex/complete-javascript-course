08db427a189aaae58b6c000f03bc548a8d6d46e9
Createlex <info@createLex.com>
2021-06-27 20:48:43 -0700

    Patricia Updaet

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const scoreElement0 = document.querySelector('#score--0');
 const scoreElement1 = document.getElementById('score--1');
 const diceElement = document.querySelector('.dice');
@@ -10,42 +9,27 @@ const btnHold = document.querySelector('.btn--hold');
 const currentElement0 = document.getElementById('current--0');
 const currentElement1 = document.getElementById('current--1');
 
-
-
-
-
 // initializes variables and reset game
 scoreElement0.textContent = 0;
 scoreElement1.textContent = 0;
 diceElement.classList.add('hidden');
 let currentScore = 0;
 
-
-
 const btnRole = document.querySelector('.btn--roll');
 const diceImage = document.querySelector('.dice');
 
 btnRoll.addEventListener('click', function () {
+  const MAXNUMBER = 6;
+  const dice = Math.floor(Math.random() * MAXNUMBER + 1);
 
-    const MAXNUMBER = 6;
-    const dice = Math.floor((Math.random() * MAXNUMBER) + 1);
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice-${dice}.png`;
 
-    diceElement.classList.remove('hidden');
-    diceElement.src = `dice-${dice}.png`;
-
-
-    if (dice !== 1) {
-
-        // add to current score
-        currentScore += dice;
-        currentElement0.textContent = currentScore;
-    } else {
-
-
-        // switch to next player
-    }
+  if (dice !== 1) {
+    // add to current score
+    currentScore += dice;
+    currentElement0.textContent = currentScore;
+  } else {
+    // switch to next player..
+  }
 });
-
-
-
-
