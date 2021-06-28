e8362d42987317ea6ec2e8837f37eea5c4e7b926
Createlex <info@createLex.com>
2021-06-27 21:29:15 -0700

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -9,8 +9,17 @@ const btnHold = document.querySelector('.btn--hold');
 const currentElement0 = document.getElementById('current--0');
 const currentElement1 = document.getElementById('current--1');
 
+const playerOne = document.querySelector('.player--0');
+const playerTwo = document.querySelector('.player--1');
+
+
+const score =[0,0];
+const currentScore = 0;
+const activePlayer = 0;
+
+    
 // initializes variables and reset game
-scoreElement0.textContent = 0;
+document.scoreElement0.textContent = 0);
 scoreElement1.textContent = 0;
 diceElement.classList.add('hidden');
 let currentScore = 0;
@@ -28,8 +37,12 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // add to current score
     currentScore += dice;
+    activePlayer = 
     currentElement0.textContent = currentScore;
   } else {
     // switch to next player..
+    document.getElementById(`current--${activePlayer}`).textContent = activePlayer;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
   }
 });
