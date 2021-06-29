5b6b594e72a4dc55dfc50800d37df243a7b4fc20
Createlex <info@createLex.com>
2021-06-28 21:13:42 -0700

    Switching Active Player
    Switching Active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+
+
 const scoreElement0 = document.querySelector('#score--0');
 const scoreElement1 = document.getElementById('score--1');
 const diceElement = document.querySelector('.dice');
@@ -9,20 +12,20 @@ const btnHold = document.querySelector('.btn--hold');
 const currentElement0 = document.getElementById('current--0');
 const currentElement1 = document.getElementById('current--1');
 
-const playerOne = document.querySelector('.player--0');
-const playerTwo = document.querySelector('.player--1');
+const playerOneElement = document.querySelector('.player--0');
+const playerTwoElement = document.querySelector('.player--1');
 
 
-const score =[0,0];
-const currentScore = 0;
-const activePlayer = 0;
+const score = [0, 0];
+let activePlayer = 0;
+let currentScore = 0;
 
-    
+
+
 // initializes variables and reset game
-document.scoreElement0.textContent = 0);
+scoreElement0.textContent = 0;
 scoreElement1.textContent = 0;
 diceElement.classList.add('hidden');
-let currentScore = 0;
 
 const btnRole = document.querySelector('.btn--roll');
 const diceImage = document.querySelector('.dice');
@@ -34,15 +37,37 @@ btnRoll.addEventListener('click', function () {
   diceElement.classList.remove('hidden');
   diceElement.src = `dice-${dice}.png`;
 
+  //starts on player 1
+  // 
+
   if (dice !== 1) {
     // add to current score
     currentScore += dice;
-    activePlayer = 
-    currentElement0.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
     // switch to next player..
-    document.getElementById(`current--${activePlayer}`).textContent = activePlayer;
+
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
     currentScore = 0;
+
+    // Toogle method for switching background collor 
+    playerOneElement.classList.toggle('player--active');
+    playerTwoElement.classList.toggle('player--active');
+
+
+    //toogle 
+
+
+    //reset both current scores to 0;
+    currentElement0.textContent = 0;
+    currentElement1.textContent = 0;
+
+
+
+
+
+
+
   }
 });
